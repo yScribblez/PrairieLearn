aa8a8b44fe52aebf8dc6a5377e76414ab8a58ff5
Nathan Walters <nwalters512@gmail.com>
2018-06-26 08:17:27 -0700

    Add table + support for issue comments

--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -1,5 +1,6 @@
 const ERR = require('async-stacktrace');
 const express = require('express');
+const moment = require('moment');
 const router = express.Router();
 const { sqlDb, sqlLoader } = require('@prairielearn/prairielib');
 
@@ -12,17 +13,35 @@ router.get('/:issueId', (req, res, next) => {
     sqlDb.queryOneRow(sql.select_issue, params, (err, result) => {
         if (ERR(err, next)) return;
         res.locals.issue = result.rows[0];
-        res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+        sqlDb.query(sql.select_comments, params, (err, result) => {
+            if (ERR(err, next)) return;
+            res.locals.comments = result.rows;
+
+            // Add human-readable relative dates to each row
+            res.locals.comments.forEach((row) => {
+                row.relative_date = moment(row.formatted_date).from(row.now_date);
+            });
+
+            res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+        });
     });
 });
 
 router.post('/:issueId', (req, res, next) => {
     if (req.body.__action === 'comment') {
         console.log(`Comment: [${req.params.issueId}] ${req.body.comment}`);
-        res.redirect(`${res.locals.urlPrefix}/issue/${req.params.issueId}`);
+        const params = {
+            user_id: res.locals.authn_user.user_id,
+            issue_id: req.params.issueId,
+            comment: req.body.comment,
+        };
+        sqlDb.queryOneRow(sql.insert_comment, params, (err) => {
+            if (ERR(err, next)) return;
+            res.redirect(req.originalUrl);
+        });
     } else if (req.body.__action === 'close_and_comment') {
         console.log(`Close and comment: [${req.params.issueId}] ${req.body.comment}`);
-        res.redirect(`${res.locals.urlPrefix}/issue/${req.params.issueId}`);
+        res.redirect(req.originalUrl);
     } else {
         next(new Error('unknown __action: ' + req.body.__action));
     }
--- pages/issue/issue.sql
+++ pages/issue/issue.sql
@@ -11,3 +11,25 @@ FROM
     LEFT JOIN users AS u ON (u.user_id = i.user_id)
 WHERE
     i.id = $issue_id;
+
+-- BLOCK select_comments
+SELECT
+    ic.*,
+    format_date_iso8601(NOW(), coalesce(ci.display_timezone, c.display_timezone)) AS now_date,
+    format_date_iso8601(ic.date, coalesce(ci.display_timezone, c.display_timezone)) AS formatted_date,
+    u.uid AS user_uid,
+    u.name AS user_name
+FROM
+    issue_comments AS ic
+    JOIN issues as i ON (i.id = ic.issue_id)
+    LEFT JOIN course_instances AS ci ON (ci.id = i.course_instance_id)
+    JOIN pl_courses AS c ON (c.id = i.course_id)
+    LEFT JOIN users AS u ON (u.user_id = ic.user_id)
+WHERE
+    ic.issue_id = $issue_id;
+
+-- BLOCK insert_comment
+INSERT INTO issue_comments
+    (issue_id, user_id, comment)
+VALUES
+    ($issue_id, $user_id, $comment);
